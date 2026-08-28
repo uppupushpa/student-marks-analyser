4e6114c0b1d8e8d547fdbc4844ea0a6a7ec3283d
uppupushpa <uppupushpa25@gmail.com>
2026-08-28 22:18:46 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Student Marks Analyser
+
+A simple python that calculates student marks, average, gradde, and pss/fail status.
+
+## Features
+
+- Accepts students name
+- Accepts marks for five subjects
+- Calculates total marks
+- Calculates Average marks
+- Assigns a grade
+- Determines pass/fail status
+- Saves student results to a CSV file
+
+## Subjects
+This project currently uses:
+
+- Python
+- Database
+- Maths
+- English
+- Computer
+
+## Technologies Used
+
+- Python
+- CSV
+- Git
+- GitHub
+
+## How to Run
+
+Open the project in VS Code.
+Open the terminal and run:
+```bash
+python main.py
---
+++ main.py
@@ -0,0 +1,57 @@
+import csv
+import os
+
+name = input("Enter student name: ")
+subjects = ["Python","Database","Maths","English","Computer"]
+marks = []
+
+for subject in subjects:
+    mark = float(input(f"Enter marks for {subjects}: "))
+    marks.append(mark)
+
+total = sum(marks)
+average = total / len(marks)
+
+if average >= 90:
+    grade = "A+"
+elif average >= 80:
+    grade = "A"
+elif average >= 70:
+    grade = "B"
+elif average >= 60:
+    grade = "B+"
+elif average >= 50:
+    grade = "D"
+else:
+    grade = "F"
+
+result = "PASS" if average >= 40 else "FAIL"
+
+print(f"Student Name : {name}")
+print(f"Total Marks  : {total}")
+print(f"Average      : {average:.2f}")
+print(f"Grade        : {grade}")
+print(f"Result       : {result}")
+
+file_name = "student_results.csv"
+file_exists = os.path.exists(file_name)
+
+with open(file_name, "a", newline="") as file:
+    writer = csv.writer(file)
+
+    if not file_exists:
+        writer.writerow([
+            "Student Name",
+            "Total Marks",
+            "Average",
+            "Grade",
+            "Results"
+        ])
+    writer.writerow([
+        name,
+        total,
+        round(average, 2),
+        grade,
+        result
+    ])
+print("\n Result has been saved to student_results.csv")
